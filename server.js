79be74be96c1e5c4c49258562341b155c7ac3e42
Piyush <piyush@mail>
2025-10-30 12:58:29 +0530

    new changes

--- server.js
+++ server.js
@@ -12,11 +12,14 @@ app.use(express.json());
 app.use('/api/rentals', rentalRoutes);
 
 const PORT = process.env.PORT || 5000;
-mongoose.connect(process.env.MONGO_URI || 'mongodb+srv://admin:<mypassword@123>@cluster0.hj0i4jp.mongodb.net/?appName=Cluster0', { useNewUrlParser:true, useUnifiedTopology:true })
-  .then(()=> {
-    console.log('MongoDB connected');
-    app.listen(PORT, ()=> console.log('Server running on port', PORT));
-  })
-  .catch(err => {
-    console.error('Mongo connect error', err);
-  });
+const MONGO_URI = process.env.MONGO_URI;
+
+mongoose.connect(MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => {
+  console.log('✅ MongoDB connected');
+  app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+})
+.catch(err => console.error('❌ Mongo connect error:', err.message));
