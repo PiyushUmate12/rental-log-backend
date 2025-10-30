e500103f607c7479e7705ac9551eb0ddc70a8e87
Piyush <piyush@mail>
2025-10-30 12:45:46 +0530

    your change message

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use('/api/rentals', rentalRoutes);
 
 const PORT = process.env.PORT || 5000;
-mongoose.connect(process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/rentaldb', { useNewUrlParser:true, useUnifiedTopology:true })
+mongoose.connect(process.env.MONGO_URI || 'mongodb+srv://admin:<mypassword@123>@cluster0.hj0i4jp.mongodb.net/?appName=Cluster0', { useNewUrlParser:true, useUnifiedTopology:true })
   .then(()=> {
     console.log('MongoDB connected');
     app.listen(PORT, ()=> console.log('Server running on port', PORT));
